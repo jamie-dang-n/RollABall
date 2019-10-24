45a7120703092488a911a5fb4f5168a43805edee
Dang_Jamie <dangj842@hsd.k12.or.us>
2019-10-24 08:45:55 -0700

    player controls and speed changed (faster, 10)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed;
+
     private Rigidbody rb;
 
     void Start ()
     {
-        rb = GetComponent<RigidBody>();
+        rb = GetComponent<Rigidbody>();
     }
   
     void FixedUpdate ()
@@ -18,7 +20,7 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement);
+        rb.AddForce (movement * speed);
 
         
     }
